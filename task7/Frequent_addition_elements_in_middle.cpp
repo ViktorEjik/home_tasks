141011e7e0697e4a4e4e1a25f2c510709fac4270
Mogilenetc Viktor <mogilenec.v.m@asvk.cs.msu.ru>
2024-12-09 07:55:08 +0300

    mend

--- task7/Frequent_addition_elements_in_middle.cpp
+++ task7/Frequent_addition_elements_in_middle.cpp
@@ -4,31 +4,44 @@
 #include <random>
 
 int main() {
-    std::map<uint64_t, uint64_t> myMap1{{0, 0}, {1000000, 1000000}};
-    std::map<uint64_t, uint64_t> myMap2{{0, 0}, {1000000, 1000000}};
-    for (uint64_t i = 1; i < 100000; ++i) {
-        myMap1.insert({i, i});
-        myMap2.insert({i, i});
-    }
-    auto it = myMap1.begin();
-    std::advance(it, 100000-1);
-    std::srand(123);
-    auto start = clock();
-    for (uint64_t i = 100000; i < 1000000; ++i) {
-        myMap1.insert(it, {100000 + std::rand()%1000000, i});
-    }
-    auto end = clock();
-    std::cout << "Hint " << (double)(end - start) / CLOCKS_PER_SEC << std::endl;
 
-    std::srand(123);
-    start = clock();
-    for (uint64_t i = 100000; i < 1000000; ++i) {
-        myMap2.insert({100000 + std::rand()%1000000, i});
-    }
-    end = clock();
+    clock_t start, end;
 
-    std::cout << "No hint " << (double)(end - start) / CLOCKS_PER_SEC << std::endl;
-//    Hint 0.582761
-//    No hint 0.503638
+    std::vector<std::pair<uint32_t, uint32_t>> test;
+    for (uint64_t i = 100000; i < 1000000; ++i){
+        test.emplace_back(100000 + std::rand()%1000000 - 3, i);
+    }
+
+    {
+        std::map<uint64_t, uint64_t> myMap1{{0,       0},
+                                            {1000000, 1000000}};
+        for (uint64_t i = 1; i < 100000; ++i) {
+            myMap1.insert({i, i});
+        }
+        auto it = myMap1.begin();
+        std::advance(it, 100000 - 1);
+
+        start = clock();
+        for (uint64_t i = 100000; i < 1000000; ++i) {
+            myMap1.insert(it, test[i - 100000]);
+        }
+        end = clock();
+        std::cout << "Hint " << (double) (end - start) / CLOCKS_PER_SEC << std::endl;
+    }
+    {
+        std::map<uint64_t, uint64_t> myMap2{{0, 0}, {1000000, 1000000}};
+        for (uint64_t i = 1; i < 100000; ++i) {
+            myMap2.insert({i, i});
+        }
+        start = clock();
+        for (uint64_t i = 100000; i < 1000000; ++i) {
+            myMap2.insert(test[i - 100000]);
+        }
+        end = clock();
+        std::cout << "No hint " << (double) (end - start) / CLOCKS_PER_SEC << std::endl;
+    }
+
+//    Hint 0.603019
+//    No hint 0.683811
     return 0;
 }
